b90ad7a54de57f6bb62e319533ae4f40c180954c
Warith Lawal <lgrimmy10@gmail.com>
2024-08-18 20:46:15 +0100

    foutheenth commit

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,10 +1,8 @@
 // src/UserContext.js
 import React, { createContext } from 'react';
 
-// Create Context
 const UserContext = createContext();
 
-// Create a Context Provider component
 export const UserProvider = ({ children, value }) => {
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
