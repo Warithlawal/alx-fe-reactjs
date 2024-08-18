80fb7fe41bb766d3002068394c971a261a8b5eef
Warith Lawal <lgrimmy10@gmail.com>
2024-08-18 18:53:39 +0100

    Fourth commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <WelcomeMessage />
-    </>
-  )
+    return (
+      <div className="App">
+          <Header />
+          <MainContent />
+          <Footer />
+      </div>
+  );
 }
 
 export default App
