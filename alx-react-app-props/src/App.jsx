9c4bee28fbc3e010980ef6e931bc88a6be6abb2f
Warith Lawal <lgrimmy10@gmail.com>
2024-08-18 20:29:06 +0100

    tenth commit

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import React from 'react';
 import ProfilePage from './ProfilePage';
-import { UserProvider } from './UserContext';
+import { UserProvider } from './components/UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
