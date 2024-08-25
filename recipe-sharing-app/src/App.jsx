45d5dd93598e3d2ee8403ad718c2b0cddf6db8f8
Warith Lawal <lgrimmy10@gmail.com>
2024-08-25 22:16:11 +0100

    recipe

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -5,6 +5,8 @@ import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
       <div>
         <h1>Recipe Manager</h1>
         <SearchBar />
+        <FavoritesList />
+        <RecommendationsList />
         <Routes>
           <Route path="/" element={<><AddRecipeForm /><RecipeList /></>} />
           <Route path="/recipe/:id" element={<RecipeDetails />} />
