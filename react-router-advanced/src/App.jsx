329f784a63bcb5f14ef1b3364325528926825e1b
Warith Lawal <lgrimmy10@gmail.com>
2024-09-01 13:33:17 +0100

    blog

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import BlogPost from './components/BlogPost';
+
 import Profile from './components/Profile';
 import Login from './components/Login';
 import UserProfile from './components/UserProfile';
 import ProtectedRoute from './components/ProtectedRoute';
+import BlogPost from './components/BlogPost';
 
 function App() {
   return (
