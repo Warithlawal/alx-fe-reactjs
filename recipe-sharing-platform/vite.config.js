42e5596563e3da5e95e89166af26dace03600619
Warith Lawal <lgrimmy10@gmail.com>
2024-09-21 14:27:47 +0100

    tail

--- recipe-sharing-platform/vite.config.js
+++ recipe-sharing-platform/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
